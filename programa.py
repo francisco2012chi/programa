16fca909ff2d5900773cd1cb79ca60252a9b4337
francisco2012chi <francisco.2012.chi@gmail.com>
2018-08-04 23:44:57 -0300

    corrição do bug da quebra de linha

--- programa.py
+++ programa.py
@@ -3,15 +3,17 @@
 def linha(x):
 	if x == 0:
 		return
-	print(x)
+	print(x, end='')
 	linha(x-1)
 
 def fc(x):
 	if x == 0:
 		return
 	linha(x)
+	print('')
 	fc(x-1)
 	if x!=1:
 		linha(x)
+		print('')
 
 fc(int(input('Digite um número: ')))

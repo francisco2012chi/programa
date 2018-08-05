ed305d97b5e64dc5740f60266fa225ddd25d318c
francisco2012chi <francisco.2012.chi@gmail.com>
2018-08-04 23:06:09 -0300

    fazer piramide

--- programa.py
+++ programa.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-def fc(x):
+
+def linha(x):
 	if x == 0:
 		return
 	print(x)
+	linha(x-1)
+
+def fc(x):
+	if x == 0:
+		return
+	linha(x)
 	fc(x-1)
 	if x!=1:
-		print(x)
+		linha(x)
 
 fc(int(input('Digite um número: ')))
